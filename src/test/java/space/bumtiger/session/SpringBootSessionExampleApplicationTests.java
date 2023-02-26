b13cb01f360ebb2eb1dfe63320fce09cbc9d14db
osparking <jbpark03@gmail.com>
2023-02-26 11:57:38 +0900

    spring starter project created

---
+++ src/test/java/space/bumtiger/session/SpringBootSessionExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package space.bumtiger.session;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSessionExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
